103b387427584976f56692523d2d147341f034c6
Evan Liu <evan.liu@gmail.com>
2026-05-04 14:06:26 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'deposit-app-v48-universal-date-picker';
+const CACHE_VERSION = 'deposit-app-v49-i18n-status-rates';
 const CACHEABLE_CDN = [
   'https://code.jquery.com/jquery-3.7.1.min.js'
 ];
