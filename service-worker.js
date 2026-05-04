07103e418c81dcae1470e250457444d24eebae8e
Evan Liu <evan.liu@gmail.com>
2026-05-04 10:26:43 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'deposit-app-v39-card-style-v36';
+const CACHE_VERSION = 'deposit-app-v41-fill-viewport-table';
 const CACHEABLE_CDN = [
   'https://code.jquery.com/jquery-3.7.1.min.js'
 ];
