44ce7d5d2935b209accf76555e0923298b222747
Evan Liu <evan.liu@gmail.com>
2026-05-05 09:02:52 +0300

    桌面端继续保持“一行查询 + 悬浮更多筛选”；移动端恢复旧的折叠查询样式，日期和剩余天数直接在折叠区内显示，不用悬浮

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'deposit-app-v54-floating-filters';
+const CACHE_VERSION = 'deposit-app-v55-mobile-fold-filters';
 const CACHEABLE_CDN = [
   'https://code.jquery.com/jquery-3.7.1.min.js'
 ];
